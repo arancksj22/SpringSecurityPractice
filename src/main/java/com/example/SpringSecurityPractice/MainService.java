323836ff3c83417707dd7c239cfc94480eaecfbb
arancksj22 <arancksj@gmail.com>
2025-09-15 22:28:10 +0530

    requests

--- src/main/java/com/example/SpringSecurityPractice/MainService.java
+++ src/main/java/com/example/SpringSecurityPractice/MainService.java
@@ -1,11 +1,12 @@
 package com.example.SpringSecurityPractice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MainService {
 
-    public String displaySecretInfo(){
+    public String displaySecretInfo(HttpServletRequest request){
         return "THIS IS SECRET INFO";
     }
 }
