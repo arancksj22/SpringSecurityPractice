d88a3a9d80c41e400401d5abfe93977e35ea6a65
arancksj22 <arancksj@gmail.com>
2025-09-15 22:02:18 +0530

    controller call

--- src/main/java/com/example/SpringSecurityPractice/Controller.java
+++ src/main/java/com/example/SpringSecurityPractice/Controller.java
@@ -1,9 +1,15 @@
 package com.example.SpringSecurityPractice;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/prod/secretInfo")
 public class Controller {
+
+    @GetMapping
+    public String secretInfoDisplay(){
+
+    }
 }
